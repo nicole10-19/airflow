4a0d1b7455f0528772af833a57b3bf8bb8dc198f
nicole10-19 <nicolefurin@gmail.com>
2026-05-08 09:16:59 +0200

    Modifiche codice

--- dags/greenhouse_pipeline.py
+++ dags/greenhouse_pipeline.py
@@ -53,9 +53,7 @@ def plot_confusion_matrix_heatmap(y_true, y_pred_dbscan, y_pred_iforest, output_
         tn, fp, fn, tp = cm.ravel()
         
         # Calcola metriche
-        precision = tp / (tp + fp) if (tp + fp) > 0 else 0
-        recall = tp / (tp + fn) if (tp + fn) > 0 else 0
-        f1 = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+        precision, recall, f1 = calc_performance_metrics(tp, fp, fn)
         
         # Crea heatmap
         sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', ax=ax,
